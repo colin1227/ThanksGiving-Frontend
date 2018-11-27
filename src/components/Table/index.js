1f5fffda60befa308c4d5bd743a649982b6f2cd3
Colin Daniel <colinmdaniel@gmail.com>
2018-11-27 09:33:59 -0600

    this is my final project now

---
+++ src/components/Table/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class Table extends Component {
+    constructor(){
+        super()
+        this.state = {
+            people: [],
+            food: []
+        }
+    }
+    render(){
+        return(
+        <h1>Hello world</h1>
+        )
+    }
+}
